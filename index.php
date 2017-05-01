1650a3fe1c1450453c8e467177cd72c396d8f1bf
Sheng Francis Xhir <delphos@Lunabel.s126>
2017-05-01 20:23:28 -0300

    actualizacion plantilla index.php

--- index.php
+++ index.php
@@ -3,19 +3,29 @@
 	<main role="main">
 		<!-- section -->
 		<section>
+			<div class="section">
+	      <div class="container">
+	        <div class="row">
+	          <div class="col-md-8">
+	            <div class="col-md-12"><h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1></div>
+	            <div class="col-md-12"><?php get_template_part('loop'); ?>
+								<?php get_template_part('pagination'); ?>
+							</div>
+	            <div class="col-md-12"></div>
+	          </div>
+	          <div class="col-md-4"><?php get_sidebar(); ?></div>
+	        </div>
+	      </div>
+	    </div>
 
-			<h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
 
-			<?php get_template_part('loop'); ?>
 
-			<?php get_template_part('pagination'); ?>
+
+
+
 
 		</section>
 		<!-- /section -->
 	</main>
-
-	<div id="lateral">
-		<?php get_sidebar(); ?>
-	</div>
 
 <?php get_footer(); ?>
