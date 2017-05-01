dbab98d717f808d9045c73f31c0ca3723dbe8b1b
Sheng Francis Xhir <delphos@Lunabel.s126>
2017-05-01 20:40:39 -0300

    actualizacion resultados de busqueda

--- search.php
+++ search.php
@@ -3,17 +3,27 @@
 	<main role="main">
 		<!-- section -->
 		<section>
+			<div class="section">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-8">
+							<div class="col-md-12"><h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1></div>
+							<div class="col-md-12"><?php get_template_part('loop'); ?>
+										<?php get_template_part('pagination'); ?></div>
+							<div class="col-md-12"></div>
+						</div>
+						<div class="col-md-4"><?php get_sidebar(); ?></div>
+					</div>
+				</div>
+			</div>
 
-			<h1><?php echo sprintf( __( '%s Search Results for ', 'html5blank' ), $wp_query->found_posts ); echo get_search_query(); ?></h1>
 
-			<?php get_template_part('loop'); ?>
 
-			<?php get_template_part('pagination'); ?>
 
 		</section>
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+
 
 <?php get_footer(); ?>
