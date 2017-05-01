e052b2db9dec8be2c687b44c92795ee9fdf40d9f
Sheng Francis Xhir <delphos@Lunabel.s126>
2017-05-01 20:42:33 -0300

    actualizacion tags

--- tag.php
+++ tag.php
@@ -3,17 +3,26 @@
 	<main role="main">
 		<!-- section -->
 		<section>
+			<div class="section">
+	      <div class="container">
+	        <div class="row">
+	          <div class="col-md-8">
+	            <div class="col-md-12"><h1><?php _e( 'Tag Archive: ', 'html5blank' ); echo single_tag_title('', false); ?></h1></div>
+	            <div class="col-md-12"><?php get_template_part('loop'); ?>
+							<?php get_template_part('pagination'); ?></div>
+	            <div class="col-md-12"></div>
+	          </div>
+	          <div class="col-md-4"><?php get_sidebar(); ?></div>
+	        </div>
+	      </div>
+	    </div>
 
-			<h1><?php _e( 'Tag Archive: ', 'html5blank' ); echo single_tag_title('', false); ?></h1>
 
-			<?php get_template_part('loop'); ?>
 
-			<?php get_template_part('pagination'); ?>
 
 		</section>
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
 
 <?php get_footer(); ?>
