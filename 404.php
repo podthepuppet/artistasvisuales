cfe701e8f52fb2b737d3f0cffb29a23a596c20e9
Sheng Francis Xhir <delphos@Lunabel.s126>
2017-05-01 20:46:56 -0300

    actualizacion error404

--- 404.php
+++ 404.php
@@ -4,13 +4,26 @@
 		<!-- section -->
 		<section>
 
-			<!-- article -->
-			<article id="post-404">
+			<div class="section">
+	      <div class="container">
+	        <div class="row">
+	          <div class="col-md-8">
+	            <div class="col-md-12"><article id="post-404">
 
-				<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
-				<h2>
-					<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
-				</h2>
+								<h1><?php _e( 'Page not found', 'html5blank' ); ?></h1>
+								<h2>
+									<a href="<?php echo home_url(); ?>"><?php _e( 'Return home?', 'html5blank' ); ?></a>
+								</h2>
+</div>
+	            <div class="col-md-12"></div>
+	            <div class="col-md-12"></div>
+	          </div>
+	          <div class="col-md-4"><?php get_sidebar(); ?></div>
+	        </div>
+	      </div>
+	    </div>
+
+			<!-- article -->
 
 			</article>
 			<!-- /article -->
@@ -19,6 +32,6 @@
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+
 
 <?php get_footer(); ?>
