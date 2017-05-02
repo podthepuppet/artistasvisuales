93f2e1d1fa7afdb166aac921976d05ca2241b82f
Sheng Francis Xhir <delphos@Lunabel.s126>
2017-05-01 22:23:23 -0300

    actualizacion demo template

--- template-demo.php
+++ template-demo.php
@@ -3,43 +3,54 @@
 	<main role="main">
 		<!-- section -->
 		<section>
+			<div class="section">
+	      <div class="container">
+	        <div class="row">
+	          <div class="col-md-8">
+	            <div class="col-md-12"><h1><?php the_title(); ?></h1>
 
-			<h1><?php the_title(); ?></h1>
+						<?php if (have_posts()): while (have_posts()) : the_post(); ?>
 
-		<?php if (have_posts()): while (have_posts()) : the_post(); ?>
+							<!-- article -->
+							<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			<!-- article -->
-			<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+								<?php the_content(); ?>
 
-				<?php the_content(); ?>
+								<?php comments_template( '', true ); // Remove if you don't want comments ?>
 
-				<?php comments_template( '', true ); // Remove if you don't want comments ?>
+								<br class="clear">
 
-				<br class="clear">
+								<?php edit_post_link(); ?>
 
-				<?php edit_post_link(); ?>
+							</article>
+							<!-- /article -->
 
-			</article>
-			<!-- /article -->
+						<?php endwhile; ?>
 
-		<?php endwhile; ?>
+						<?php else: ?>
 
-		<?php else: ?>
+							<!-- article -->
+							<article>
 
-			<!-- article -->
-			<article>
+								<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
 
-				<h2><?php _e( 'Sorry, nothing to display.', 'html5blank' ); ?></h2>
+							</article>
+							<!-- /article -->
 
-			</article>
-			<!-- /article -->
+						<?php endif; ?></div>
+	            <div class="col-md-12"></div>
+	            <div class="col-md-12"></div>
+	          </div>
+	          <div class="col-md-4"><?php get_sidebar(); ?></div>
+	        </div>
+	      </div>
+	    </div>
 
-		<?php endif; ?>
 
 		</section>
 		<!-- /section -->
 	</main>
 
-<?php get_sidebar(); ?>
+
 
 <?php get_footer(); ?>
