9cdf82f78b3174f0b95234c6dad13febf99280bf
Sheng Francis Xhir <delphos@Lunabel.s126>
2017-05-01 23:41:25 -0300

    actualizacion inicio, en espera de agregar imagen del arbol

--- index.php
+++ index.php
@@ -3,26 +3,18 @@
 	<main role="main">
 		<!-- section -->
 		<section>
+
 			<div class="section">
-	      <div class="container">
-	        <div class="row">
-	          <div class="col-md-8">
-	            <div class="col-md-12"><h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1></div>
-	            <div class="col-md-12"><?php get_template_part('loop'); ?>
-								<?php get_template_part('pagination'); ?>
+						<div class="container">
+							<div class="row">
+								<div class="col-md-12">
+									<center><h1><?php _e( 'Latest Posts', 'html5blank' ); ?></h1>
+										<?php get_template_part('loop'); ?>
+											<?php get_template_part('pagination'); ?></center>
+								</div>
 							</div>
-	            <div class="col-md-12"></div>
-	          </div>
-	          <div class="col-md-4"><?php get_sidebar(); ?></div>
-	        </div>
-	      </div>
-	    </div>
-
-
-
-
-
-
+						</div>
+					</div>
 
 		</section>
 		<!-- /section -->
